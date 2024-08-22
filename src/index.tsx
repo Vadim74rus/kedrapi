3c25f483f22abc11347c4bc84bf0ec0cf22b515c
Vadim74rus <167786265+Vadim74rus@users.noreply.github.com>
2024-08-22 23:41:31 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const router = express.Router();
+const UserController = require('../controllers/user-controller');
+const PostController = require('../controllers/post-controller');
+const FollowController = require('../controllers/follow-controller');
+const LikeController = require('../controllers/like-controller');
+const CommentController = require('../controllers/comment-controller');
+const { authenticateToken } = require('../middleware/auth');
+const multer = require('multer');
+const path = require('path');
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const uploadDestination = path.join(__dirname, '..', 'uploads');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Показываем, где хранить загружаемые файлы
+const storage = multer.diskStorage({
+  destination: uploadDestination,
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Логирование запросов
+router.use((req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+});
+
+// Роуты User
+router.post('/register', UserController.register);
+router.post('/login', UserController.login);
+router.get('/current', authenticateToken, UserController.current);
+router.get('/users/:id', authenticateToken, UserController.getUserById);
+router.put('/users/:id', authenticateToken, upload.single('avatar'), UserController.updateUser);
+
+// Роуты Post
+router.post('/posts', authenticateToken, PostController.createPost);
+router.get('/posts', authenticateToken, PostController.getAllPosts);
+router.get('/posts/:id', authenticateToken, PostController.getPostById);
+router.delete('/posts/:id', authenticateToken, PostController.deletePost);
+
+// Роуты подписки
+router.post('/follow', authenticateToken, FollowController.followUser);
+router.delete('/unfollow/:id', authenticateToken, FollowController.unfollowUser);
+
+// Роуты лайков
+router.post('/likes', authenticateToken, LikeController.likePost);
+router.delete('/likes/:id', authenticateToken, LikeController.unlikePost);
+
+// Роуты комментариев
+router.post('/comments', authenticateToken, CommentController.createComment);
+router.delete('/comments/:id', authenticateToken, CommentController.deleteComment);
+
+module.exports = router;
